151678fb076d4d2c1724feb73476dbe42b364d12
dgaujero <deion@aujero.net>
2021-06-21 15:29:28 -0400

    added react client and devdependcies

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('This route works')
 })
 
+app.use('/test', require('./routes/test'))
+
 app.listen(PORT, (req, res) => {
     console.log(`Listening on ${PORT}`)
 } )
+
+// Accept a POST request to the route “/test”, which accepts one argument “string_to_cut”
+// Return a JSON object with the key “return_string” and a string containing every third letter from the original string
